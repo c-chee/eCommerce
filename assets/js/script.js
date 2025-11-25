f5604e139ee41bff8c7323f5d2cb5d99175ee0b7
Christine Chee <christinechee2000@gmail.com>
2025-11-25 08:56:13 -0500

    added footer

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+
+// === Hamburger Menu ===
+const hiddenNav   = document.getElementById('hidden-nav');
+const hamburgerNav   = document.getElementById('hamburger-btn');
+const closeNavBtn    = document.getElementById('nav-close-btn');
+const navOverlay  = document.getElementById('nav-overlay'); // Allows close when clicking outside of hidden nav
+
+// Open menu
+function openHiddenNav() {
+    hiddenNav.classList.remove('close');
+    hiddenNav.classList.add('open');
+    navOverlay.classList.add('show');
+}
+
+// Close menu
+function closeHiddenNav() {
+    hiddenNav.classList.remove('open');
+    hiddenNav.classList.add('close');
+    navOverlay.classList.remove('show');
+}
+
+// Button events
+hamburgerNav.addEventListener('click', openHiddenNav);
+closeNavBtn.addEventListener('click', closeHiddenNav);
+navOverlay.addEventListener('click', closeHiddenNav);
