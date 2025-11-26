2a2c5d0aa8de6c67d8ec32d2743b2b7393413a43
Christine Chee <christinechee2000@gmail.com>
2025-11-26 05:42:46 -0500

    adde contact form with ty messsage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 
-
 const hiddenNav = document.getElementById('hidden-nav');
 const hamburgerNav = document.getElementById('hamburger-btn');
 const closeNavBtn = document.getElementById('nav-close-btn');
 const navOverlay = document.getElementById('nav-overlay'); // Allows close when clicking outside of hidden nav
 
+const aboutSection = document.getElementById('about-section');
 const scrollToAbout = document.getElementById('scroll-to-about'); 
 
 const grid = document.getElementById('product-grid');
+
+const contactSection = document.querySelector('.contact-section');
+const contactForm = document.getElementById('contact-form');
+const validationMessage = document.getElementById('validation-message');
+validationMessage.style.color = 'red';
+const thankyouMessage = document.getElementById('thankyou-section');
 
 
 // === Hamburger Menu ===
@@ -35,7 +41,6 @@ navOverlay.addEventListener('click', closeHiddenNav);
 // Only on home page, so a check is needed that that an error does not occur
 if (scrollToAbout) { 
     scrollToAbout.addEventListener('click', () => {
-        const aboutSection = document.getElementById('about-section');
         const offset = 130;
         const targetPosition = aboutSection.offsetTop - offset;
 
@@ -70,3 +75,44 @@ if (grid) {
     })
     .catch(err => console.error("Error loading products:", err));
 }
+
+// === Contact Form ===
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Ensures the form doesn’t submit if validation fails
+
+    // -- Grab values --
+    const formName = document.getElementById('name-input').value.trim(); // - Trim removes extra space
+    const formEmail = document.getElementById('email-input').value.trim();
+    const formMessage = document.getElementById('message-input').value.trim();
+
+    // -- Validations --
+    // Extra checks for if a required field is left empty or if only spaces are typed
+    if (formName === '' || formEmail === '' || formMessage === '') {
+        validationMessage.textContent = 'Form is left empty! Please fill out required field.';
+        return;
+    }
+
+    // Checks for email formatting
+    const emailFormat = /\S+@\S+\.\S+/;
+
+    // If the email does not match the formatt, display error
+    if(!emailFormat.test(formEmail)) {
+        validationMessage.textContent = 'Please enter a valid email.'
+        return;
+    }
+
+    // Clear any validatiion messages
+    validationMessage.textContent = '';
+
+    // Swap visibility between form and thankyou section
+    contactSection.style.display = 'none';
+    thankyouMessage.style.display = 'block'; 
+
+    // Revert back to normal after 3 seconds
+    setTimeout(() => {
+        thankyouMessage.style.display = 'none';
+        contactSection.style.display = 'block';
+        contactForm.reset();
+    }, 3000);
+
+});
